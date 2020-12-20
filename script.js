ec100a515bcf4220102505574fc313000fde0594
Calvin <calmanning@gmail.com>
2020-12-20 11:25:31 -0800

    added some id's to the HTML, beginning work on the JS, getting ready to melt my brain.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("it's a start.");
+
+// TODO: create get API function to pull data from openweather
+    // TODO: make sure you parse the url to descern how to add the "city" variable in the search.
+
+    
