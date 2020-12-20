caa81e281b3baf6a1295bc3b7bc6641604bbb39e
Calvin <calmanning@gmail.com>
2020-12-20 12:18:49 -0800

    working on the API request and the user city-input. getting a 400 bad error request. I think I'll using the wrong variable placement, but we'll see.

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ console.log("it's a start.");
 // TODO: create get API function to pull data from openweather
     // TODO: make sure you parse the url to descern how to add the "city" variable in the search.
 
-    
+    // API URL: api.openweathermap.org/data/2.5/weather?q={city name}&appid=e996e591a7658566c640a16175cb40ab
+
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+var userCity = $("#city");
+
+
+$("#city").on("click", function(){
+    console.log(userCity)
+    $.get(queryURL).then(function(response){
+        console.log(response);
+    })
+
+})
