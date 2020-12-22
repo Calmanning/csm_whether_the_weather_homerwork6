83402ab9188f523ac7b0fbd4891e1a36e23cc1ad
Calvin <calmanning@gmail.com>
2020-12-22 08:47:54 -0800

    Late update! Improved JS functions: dynamic 5-day forecast added...needs clean up. Still working on getting the icons to populate and the prev localdata to load.

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ console.log("it's a start.");
 
 // API URL: api.openweathermap.org/data/2.5/weather?q={city name}&appid=e996e591a7658566c640a16175cb40ab
 
-
+var cityEl = $("#today-city");
+var tempEl = $("#today-temp");
+var humiEl = $("#today-hu");
+var windEl = $("#today-wind");
+var uvEl = $("#today-uv");
 
 var userCity = $("#city");
-var city = "";  //just testing the queryURL and the click event.
+var city = "";
+var searchHist = [];
+var clearHist = $("#clear")
+
 
 function getTheWheather(event) {
+    event.preventDefault();//this isn't working
     if (userCity.val() !== "") {
         var city = userCity.val();
         console.log(city)
 
+        //begin the api request
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413&units=imperial"
 
         $.get(queryURL).then(function (response) {
@@ -26,6 +35,10 @@ function getTheWheather(event) {
             console.log(`humidity: ${response.main.humidity}`);
             console.log(`windspeed: ${response.wind.speed}`);
 
+            cityEl.text(response.name);
+            tempEl.text(response.main.temp);
+            humiEl.text(response.main.humidity);
+            windEl.text(response.wind.speed);
 
             // getting the lat long info for the uv index
             var cityLat = JSON.stringify(response.coord.lat);
@@ -33,23 +46,72 @@ function getTheWheather(event) {
             var cityLon = JSON.stringify(response.coord.lon);
             console.log(cityLon)
 
-            uvMe(response.coord.lat, response.coord.lon);
-            
-        })
-        
-        }
-        //uv index lat/long api call
-    var uvURL ="https://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat="+cityLat+"&lon="+cityLon;
-    //http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
 
-    function uvMe(cityLat,cityLon) {
-        $.get(uvURL).then(function (response) {
-            console.log(response);
-        })
-    }
-   
+
+            var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat=" + cityLat + "&lon=" + cityLon;
+            //http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+
+            $.get(uvURL).then(function (response) {
+                console.log(response);
+                uvEl.text(response.value);
+            })
+
+            fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+                // "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413"
+
+                $.get(fiveDayURL).then(function (response) {
+                    console.log(response)
+                    for (var i = 0; i < response.list.length; i++) {
+                        if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                            console.log("five day array:" + response.list[i].main.temp);
+                            console.log("fiveday symbol:" + response.list[i].weather[0].icon.value);
+                            console.log("dates:" + response.list[i].weather)
+                            //var day = //find the dates
+                            var foreTemp = $("<p>").text("Temp:" + response.list[i].main.temp);
+                            var foreHumid = $("<p>").text("Humidity:" + response.list[i].main.humidity);
+                            var foreIcon = ("icon:" + response.list[i].weather[0].icon.stringify)
+                        var container = $(".row");
+                        var card = $("<div>").addClass("col-sm-2 thumbnail bg-info rounded five-day");
+                        //var cardDate = ("<p>").text(day);
+                        var cardIconURL = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+foreIcon+"@2x.png");
+                           
+                            container.append(card.append(foreTemp, foreHumid))//add card date & icon
+                            //container.append
+
+                        }
+
+                    }
+
+                })
+
+
+
+            if (response.name !== "") {
+                console.log(response.name);
+                searchedCity = (response.name);
+                searchHist.push(searchedCity);
+                console.log(searchHist);
+                localStorage.setItem("city", JSON.stringify(searchHist));
+                makeButton(searchedCity);
+            }
+
+
+        }) //end of the first, current-weather api request 
+
+    } //end of the "if" statement that will run the current-weather api
+
+} //end of the getTheWeather function
+
+function makeButton(searchedCity) {
+    var prevCityButton = $("<button>");
+    $(prevCityButton).text(searchedCity);
+    $("#past-cities").prepend(prevCityButton);
 }
 
+//     //calling the UV api...or at least trying to.
+// uvMe(response.coord.lat,response.coord.lon);
+
+// 
 
 
 
