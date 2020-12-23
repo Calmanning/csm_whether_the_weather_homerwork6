d9ea3ab91ce1a439509a3a08ef672400f92e22c8
Calvin <calmanning@gmail.com>
2020-12-22 16:55:07 -0800

    Last update? Got five-day functioning (with time and icons).Button history and re-search function working.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 console.log("it's a start.");
 
 // TODO: create get API function to pull data from openweather
@@ -10,23 +12,29 @@ var tempEl = $("#today-temp");
 var humiEl = $("#today-hu");
 var windEl = $("#today-wind");
 var uvEl = $("#today-uv");
-
+var iconEl =$("#weather-icon")
 var userCity = $("#city");
 var city = "";
 var searchHist = [];
 var clearHist = $("#clear")
+var fiveDayEl = $("#fiveDayHome")
 
+//attempting to recall search history
+//function find(searchHist){
+//    for (var i=0; i<searchHist.length; i++){
+//       if
+//} //end of search history recall function
 
-function getTheWheather(event) {
-    event.preventDefault();//this isn't working
-    if (userCity.val() !== "") {
-        var city = userCity.val();
-        console.log(city)
-
+function getTheWheather(place) {
+        console.log(place)
+        //Clear out any previous search results
+        iconEl.empty();
+        fiveDayEl.empty();
         //begin the api request
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413&units=imperial"
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=166a433c57516f51dfab1f7edaed8413&units=imperial"
 
         $.get(queryURL).then(function (response) {
+            //everytime I click I need to clear the previous five-day(all of 'em) and city icons
             console.log(response);
 
             //what follows is a bunch of console.logs to show where the information I need is.
@@ -35,7 +43,11 @@ function getTheWheather(event) {
             console.log(`humidity: ${response.main.humidity}`);
             console.log(`windspeed: ${response.wind.speed}`);
 
+            todayIconCode = (response.weather[0].icon)
+                console.log("symbol" + todayIconCode)
+            todayIconURL = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+todayIconCode+".png");
             cityEl.text(response.name);
+            iconEl.append(todayIconURL);
             tempEl.text(response.main.temp);
             humiEl.text(response.main.humidity);
             windEl.text(response.wind.speed);
@@ -56,26 +68,26 @@ function getTheWheather(event) {
                 uvEl.text(response.value);
             })
 
-            fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
+            fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q="+place+"&units=imperial&appid=166a433c57516f51dfab1f7edaed8413"
                 // "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413"
 
                 $.get(fiveDayURL).then(function (response) {
                     console.log(response)
                     for (var i = 0; i < response.list.length; i++) {
                         if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                            console.log("this is going to iterate?");
                             console.log("five day array:" + response.list[i].main.temp);
-                            console.log("fiveday symbol:" + response.list[i].weather[0].icon.value);
-                            console.log("dates:" + response.list[i].weather)
-                            //var day = //find the dates
+                            console.log("fiveday symbol:" + response.list[i].weather[0].icon);
+                            console.log("dates:" + response.list[i].dt_txt)
+                            var cardDay = $("<p>").text(response.list[i].dt_txt);
                             var foreTemp = $("<p>").text("Temp:" + response.list[i].main.temp);
                             var foreHumid = $("<p>").text("Humidity:" + response.list[i].main.humidity);
-                            var foreIcon = ("icon:" + response.list[i].weather[0].icon.stringify)
-                        var container = $(".row");
+                            var foreIcon = (response.list[i].weather[0].icon)
+                        var container = $("#fiveDayHome");
                         var card = $("<div>").addClass("col-sm-2 thumbnail bg-info rounded five-day");
-                        //var cardDate = ("<p>").text(day);
                         var cardIconURL = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+foreIcon+"@2x.png");
                            
-                            container.append(card.append(foreTemp, foreHumid))//add card date & icon
+                            container.append(card.append(cardDay, cardIconURL, foreTemp, foreHumid))//add card date & icon
                             //container.append
 
                         }
@@ -86,34 +98,53 @@ function getTheWheather(event) {
 
 
 
-            if (response.name !== "") {
-                console.log(response.name);
-                searchedCity = (response.name);
-                searchHist.push(searchedCity);
-                console.log(searchHist);
-                localStorage.setItem("city", JSON.stringify(searchHist));
-                makeButton(searchedCity);
-            }
+            }) //end of the first, current-weather api request 
 
-
-        }) //end of the first, current-weather api request 
-
-    } //end of the "if" statement that will run the current-weather api
+    //} //end of the "if" statement that will run the current-weather api
 
 } //end of the getTheWeather function
+
+
 
 function makeButton(searchedCity) {
     var prevCityButton = $("<button>");
     $(prevCityButton).text(searchedCity);
+    prevCityButton.addClass("oldCity");
+    //prevCityButton.attr("value", searchedCity);
     $("#past-cities").prepend(prevCityButton);
+    
 }
 
-//     //calling the UV api...or at least trying to.
-// uvMe(response.coord.lat,response.coord.lon);
-
+function clearHistory(event){
+    event.preventDefault();
+    searchHist = [];
+    localStorage.removeItem("city");
+    document.location.reload();
+}
 // 
+$("#past-cities").on("click", "button", function(){
+    console.log("clicked");
+        var reSearch = $(this).html();
+    console.log(reSearch);
+        getTheWheather(reSearch);
+})
+//
+$("#search").on("click",function(){
+    var place = $("#city").val();
+    $("#city").val("");
+console.log("this is place: " + place);
+    getTheWheather(place);
+    if (place !== "") {
+        searchHist.push(place);
+        console.log(searchHist);
+        localStorage.setItem("city", JSON.stringify(searchHist));
+        makeButton(place);
+
+    }
+})
 
 
 
+$("#clear").on("click", clearHistory);
 
-$("#search").on("click", getTheWheather);
+}) //Document ready end.
